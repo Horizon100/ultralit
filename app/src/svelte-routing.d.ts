84c40fdf4214e61c87dbae3090a92ccf815dc32a
ruponez <ruponez@gmail.com>
2024-07-22 17:51:34 +0300

    Initial commit

--- app/src/svelte-routing.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'svelte-routing' {
-    import { SvelteComponent } from 'svelte';
-    
-    export class Router extends SvelteComponent {}
-    export class Link extends SvelteComponent {}
-    export class Route extends SvelteComponent {}
-  }
