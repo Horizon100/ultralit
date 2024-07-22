434f6a8435f89c9b7612f6b59cdaf8bc76b54984
ruponez <ruponez@gmail.com>
2024-07-22 08:50:10 +0300

    Landing page initiation

---
+++ app/src/svelte-routing.d.ts
@@ -0,0 +1,7 @@
+declare module 'svelte-routing' {
+    import { SvelteComponent } from 'svelte';
+    
+    export class Router extends SvelteComponent {}
+    export class Link extends SvelteComponent {}
+    export class Route extends SvelteComponent {}
+  }
